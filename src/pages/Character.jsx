69366569aee0fb66d1224e43018afed6845ec3ac
DeMarcus Brown <demarcus.m.brown@gmail.com>
2022-10-15 00:18:40 -0400

    initial files and router switch

---
+++ src/pages/Character.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Character(props){
+  return (
+    <div>
+        Your in Characters
+    </div>
+  )
+}
+
+export default Character
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Landing(){
+
+  return (
+    <div>
+        <h3>LANDING PAGE</h3>
+    </div>
+  )
+}
+
+export default Landing
